068689277aea3f4ecb3bceb76ae4e1dfae841365
tonimatoni <matts.mata@gmail.com>
2022-03-05 15:45:24 +0100

    unutrasnji trouglovi

--- script.js
+++ script.js
@@ -3,58 +3,87 @@ const tri = document.getElementById("triangle").getContext("2d");
 const beliProstor = 300 - (300 * Math.sqrt(3)) / 2;
 
 const nacrtajTrougao = (trougao, fillStyle) => {
-  const { prvaTacka, drugaTacka, trecaTacka } = trougao;
+    const { prvaTacka, drugaTacka, trecaTacka } = trougao;
 
-  tri.fillStyle = fillStyle;
-  tri.beginPath();
-  tri.moveTo(prvaTacka.x, prvaTacka.y);
-  tri.lineTo(drugaTacka.x, drugaTacka.y);
-  tri.lineTo(trecaTacka.x, trecaTacka.y);
-  tri.closePath();
-  tri.fill();
+    tri.fillStyle = fillStyle;
+    tri.beginPath();
+    tri.moveTo(prvaTacka.x, prvaTacka.y);
+    tri.lineTo(drugaTacka.x, drugaTacka.y);
+    tri.lineTo(trecaTacka.x, trecaTacka.y);
+    tri.closePath();
+    tri.fill();
 };
 
 const visinaTrougla = (stranica) => {
-  return (stranica * Math.sqrt(3)) / 2;
+    return (stranica * Math.sqrt(3)) / 2;
 };
 
-const func = (duzina, tackaVrh) => {
-  const visina = visinaTrougla(duzina);
-  let prvaTacka = {
-    x: tackaVrh.x - duzina / 4,
-    y: beliProstor + visina / 2,
-  };
-  let drugaTacka = {
-    x: tackaVrh.x + duzina / 4,
-    y: beliProstor + visina / 2,
-  };
-  let trecaTacka = {
-    x: tackaVrh.x,
-    y: duzina,
-  };
+const izracunajDuzinu = ({ x1, y1 }, { x2, y2 }) => {
+    let y = x2 - x1;
+    let x = y2 - y1;
 
-  const trougao = {
-    prvaTacka: prvaTacka,
-    drugaTacka: drugaTacka,
-    trecaTacka: trecaTacka,
-  };
+    return Math.sqrt(x * x + y * y);
+}
+const func = (duzina, tackaVrh, brojac) => {
+    const visina = visinaTrougla(duzina);
+    let prvaTacka = {
+        x: tackaVrh.x - duzina / 4,
+        y: beliProstor + visina / 2,
+    };
+    console.log(prvaTacka)
 
-  nacrtajTrougao(trougao, "red");
+    let drugaTacka = {
+        x: tackaVrh.x + duzina / 4,
+        y: beliProstor + visina / 2,
+    };
+    let trecaTacka = {
+        x: tackaVrh.x,
+        y: beliProstor + visina,
+    };
+
+    const trougao = {
+        prvaTacka: prvaTacka,
+        drugaTacka: drugaTacka,
+        trecaTacka: trecaTacka,
+    };
+
+    nacrtajTrougao(trougao, "red");
+    const tackaVrh2 = {
+        x: trougao.trecaTacka.x,
+        y: beliProstor
+    };
+
+    const tackaVrh3 = {
+        x: trougao.prvaTacka.x,
+        y: trougao.prvaTacka.y,
+    };
+
+
+    const tackaVrh4 = {
+        x: trougao.trecaTacka.x,
+        y: beliProstor
+    };
+
+    if (brojac) {
+        func(duzina / 2, tackaVrh2, brojac - 1);
+        // func(duzina / 2, tackaVrh3, brojac - 1);
+        // func(duzina / 2, tackaVrh4, brojac - 1);
+    }
 };
 
 const trougao = {
-  prvaTacka: {
-    x: 0,
-    y: 300,
-  },
-  drugaTacka: {
-    x: 150,
-    y: beliProstor,
-  },
-  trecaTacka: {
-    x: 300,
-    y: 300,
-  },
+    prvaTacka: {
+        x: 0,
+        y: 300,
+    },
+    drugaTacka: {
+        x: 150,
+        y: beliProstor,
+    },
+    trecaTacka: {
+        x: 300,
+        y: 300,
+    },
 };
 
 nacrtajTrougao(trougao, "blue");
@@ -63,8 +92,8 @@ const duzinaGlavnog = 300;
 const visina = (duzinaGlavnog * Math.sqrt(3)) / 2;
 
 const vrhovnaTacka = {
-  x: duzinaGlavnog / 2,
-  y: beliProstor,
+    x: duzinaGlavnog / 2,
+    y: beliProstor,
 };
 
-func(duzinaGlavnog, vrhovnaTacka);
+func(duzinaGlavnog, vrhovnaTacka, 1);
